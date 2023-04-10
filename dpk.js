4b3df526d47850201b9a5620f56e0ac965f4aca5
dario <dariogabalec@gmail.com>
2023-04-10 18:00:03 -0300

    refactor: improved readability and modularization

--- dpk.js
+++ dpk.js
@@ -1,28 +1,50 @@
-const crypto = require("crypto");
+const crypto = require('crypto')
 
-exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+const TRIVIAL_PARTITION_KEY = '0'
+const MAX_PARTITION_KEY_LENGTH = 256
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+/**
+ * Hash the given data using the sha3-512 algorithm.
+ * @param {string} data - The data to be hashed.
+ * @returns {string} - The resulting hashed data.
+ */
+function hashData(data) {
+  return crypto.createHash('sha3-512').update(data).digest('hex')
+}
+
+/**
+ * Ensure that the input candidate is a string.
+ * @param {*} candidate - The value to be converted into a string.
+ * @returns {string} - The resulting string.
+ */
+function ensureIsString(candidate) {
+  if (typeof candidate !== 'string') {
+    return JSON.stringify(candidate)
   }
+  return candidate
+}
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
+/**
+ * Truncate the input candidate to the maximum allowed length by hashing it if necessary.
+ * @param {string} candidate - The input candidate to be truncated.
+ * @returns {string} - The truncated or hashed candidate.
+ */
+function truncateToMaxLength(candidate) {
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
+    return hashData(candidate)
   }
-  return candidate;
-};
+  return candidate
+}
+
+/**
+ * Generate a deterministic partition key for the given event.
+ * @param {Object} event - The event for which the partition key is to be generated.
+ * @returns {string} - The generated partition key.
+ */
+exports.deterministicPartitionKey = (event) => {
+  if (!event) return TRIVIAL_PARTITION_KEY
+
+  const candidate = event.partitionKey ? event.partitionKey : hashData(ensureIsString(event))
+
+  return truncateToMaxLength(candidate)
+}
